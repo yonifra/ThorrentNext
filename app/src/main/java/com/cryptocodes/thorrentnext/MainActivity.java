35b43b7e73f233c7dbb68ddd669b754c187cef47
Jonathan Fraimorice <yonifra@gmail.com>
2018-06-16 23:46:02 +0300

    Modified nav menu

--- app/src/main/java/com/cryptocodes/thorrentnext/MainActivity.java
+++ app/src/main/java/com/cryptocodes/thorrentnext/MainActivity.java
@@ -110,14 +110,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_movies) {
             getItemsByCategory("films");
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        } else if (id == R.id.nav_tvseries) {
+            getItemsByCategory("tv");
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
