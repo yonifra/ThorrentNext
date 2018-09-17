b7eeb05df85da2b57ea9b449c9ee4deeef93ae52
Jonathan Fraimorice <yonifra@gmail.com>
2018-09-17 12:16:04 +0300

    New details activity

--- app/src/main/java/com/cryptocodes/thorrentnext/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/cryptocodes/thorrentnext/activities/MovieDetailsActivity.java
@@ -2,11 +2,21 @@ package com.cryptocodes.thorrentnext.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cryptocodes.thorrentnext.R;
+import com.cryptocodes.thorrentnext.RetrieveTmdbData;
+import com.squareup.picasso.Picasso;
+import com.uwetrottmann.tmdb2.entities.BaseMovie;
+
+import java.util.concurrent.ExecutionException;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    private static final String tmdbBaseImageUrl = "https://image.tmdb.org/t/p/w";
+    private static final String smallUrlSuffix = "185";
+    private static final String largeUrlSuffix = "500";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +24,20 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.movie_details);
 
         TextView title = findViewById(R.id.movie_title);
+        TextView description = findViewById(R.id.description_text_view);
+        ImageView poster = findViewById(R.id.moviePosterImage);
 
-        title.setText(getIntent().getStringExtra("title"));
+        String movieTitle = getIntent().getStringExtra("title");
+        title.setText(movieTitle);
+
+        try {
+            BaseMovie m = new RetrieveTmdbData().execute(movieTitle).get();
+
+            title.setText(m.original_title);
+            Picasso.get().load(tmdbBaseImageUrl + largeUrlSuffix + m.poster_path).into(poster);
+            description.setText(m.overview);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
